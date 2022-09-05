14f83cc494e782e2df21bc0f0ca560ea9e5724fa
adithya <adithyavallampati@gmail.com>
2022-09-05 19:43:15 +0530

    changed  get datapoint and getratio

--- client.py
+++ client.py
@@ -35,14 +35,14 @@ def getDataPoint(quote):
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
+	price = (bid_price+ask_price)/2
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
 	""" ------------- Update this function ------------- """
 	""" Also create some unit tests for this function in client_test.py """
-	return 1
+	return price_a/price_b
 
 # Main
 if __name__ == "__main__":
